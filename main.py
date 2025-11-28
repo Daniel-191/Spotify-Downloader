99f3fd968db862f329b29e70e0c8152558270d67
Daniel <145261080+Daniel-191@users.noreply.github.com>
2025-11-28 17:03:30 +0000

    removed two comments

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ Spotify Downloader - Main Entry Point
 
 Python Version: 3.13.7
 Collaborators: Daniel-191, ShellDrak3, JayM2F
-Project Start Date: 23/11/25
-Project End Date: xx/xx/xx
 
 PLEASE SEE DOCUMENTATION.py FOR DOCS
 """
